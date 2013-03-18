741f8376362630c7033dbb64c92b702808be8bd5
Tiankui <eric.prototype@gmail.com>
2013-03-18 22:39:37 +0800

    add cluster

--- app.js
+++ app.js
@@ -2,50 +2,53 @@
  * Module dependencies.
  */
 
-var express = require('express')
-    , sysConfig = require('./config.js')
-    , routes = require('./routes')
-    , http = require('http')
-    , path = require('path');
+var express = require('express'),
+sysConfig = require('./config.js'),
+routes = require('./routes'),
+http = require('http'),
+path = require('path'),
+cluster = require('./cluster');
 
 var app = express();
 
 app.configure(function () {
-    app.set('port', process.env.PORT || sysConfig.LISTEN_PORT);
-    app.set('server_host',sysConfig.SERVER);
-    app.set('views', __dirname + '/app/views');
-    app.set('view engine', 'ejs');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser());
-    app.use(express.session({ secret: sysConfig.SESSION_SECRET_KEY}));
-    app.use(app.router);
-//  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-    app.use('/lib', express.static(__dirname + '/app/lib'));
-    app.use('/static', express.static(__dirname + '/app/dist'));
-    app.use('/static', express.static(__dirname + '/app/img'));
-    app.use('/test', express.static(__dirname + '/codeLab'));
+  app.set('port', process.env.PORT || sysConfig.LISTEN_PORT);
+  app.set('server_host',sysConfig.SERVER);
+  app.set('views', __dirname + '/app/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: sysConfig.SESSION_SECRET_KEY}));
+  app.use(app.router);
+  //  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use('/lib', express.static(__dirname + '/app/lib'));
+  app.use('/static', express.static(__dirname + '/app/dist'));
+  app.use('/static', express.static(__dirname + '/app/img'));
+  app.use('/test', express.static(__dirname + '/codeLab'));
 });
 
 app.set('env', 'development');
 app.configure('development', function () {
-    console.log('development mode');
+  console.log('development mode');
 });
 app.configure('production', function () {
-    console.log('production mode');
+  console.log('production mode');
 });
 
 //加载路由
 routes(app);
 
-/**
- http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});*/
-var server = http.createServer(app);
-var socketService = require('./socket')(server);
-server.listen(app.get('port'), function () {
+cluster(function () {
+  /**
+    http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
+    });*/
+  var server = http.createServer(app);
+  var socketService = require('./socket')(server);
+  server.listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+  });
 });
