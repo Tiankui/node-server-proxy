fdb372c587929cb405aa83631632583060dc6196
Tiankui <eric.prototype@gmail.com>
2013-03-20 15:30:13 +0800

    基本样式

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 var main_module = require('./main');
 module.exports = function(app){
-    getRoutes(app,main_module.routings);
+  getRoutes(app,main_module.routings);
 };
 
 var getRoutes = function(app,routings){
-    //加载配置文件routing.js到变量routings. [/scripts/routing.js]
-    for(var r in routings){
-            //var pf = require(__dirname + routings[r].file)[routings[r].processFunction];
-            var pf = routings[r].processFunction;
-            if(routings[r].method == 'get')
-                app.get(r, pf);
-            else if(routings[r].method == 'post')
-                app.post(r,pf);
-            else
-                app.all(r, pf);
-    }
-}
+  //加载配置文件routing.js到变量routings. [/scripts/routing.js]
+  for(var r in routings){
+    //var pf = require(__dirname + routings[r].file)[routings[r].processFunction];
+    var pf = routings[r].processFunction;
+    if(routings[r].method == 'get')
+      app.get(r, pf);
+    else if(routings[r].method == 'post')
+      app.post(r,pf);
+    else
+      app.all(r, pf);
+  }
+};
--- routes/main/index.js
+++ routes/main/index.js
@@ -1,10 +1,10 @@
 var login_routes = require('./login/routes');
 var getdata_routes = require('./getdata/routes');
 exports.routings = {
-        //首页
-        '/':{method:'get',processFunction:require('./main')()},
-        //登录
-        '/login':{method:'get',processFunction:login_routes.login},
-        //获取数据
-        '/getdata':{method:'get',processFunction:getdata_routes.getData}
-    };
+  //首页
+  '/':{method:'get',processFunction:require('./main')()},
+  //登录
+  '/login':{method:'get',processFunction:login_routes.login},
+  //获取数据
+  '/getdata':{method:'get',processFunction:getdata_routes.getData}
+};
