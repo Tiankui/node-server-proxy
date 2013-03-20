f083a82ae09a19c85c41a80417cf701c93257bca
Tiankui <eric.prototype@gmail.com>
2013-03-20 16:30:40 +0800

    json接口

---
+++ routes/main/fe-data/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "title":"Family-百度",
+  "nav":[
+    {name:"首页",href:"#"},
+    {name:"新闻中心",href:"#"},
+    {name:"办公信息",href:"#",child:[{name:"制度文档",href:"#"},{name:"制度文档",href:"#"},{name:"制度文档",href:"#"}]},
+    {name:"制度文档",href:"#"},
+    {name:"百度生活",href:"#"},
+    {name:"百度学院",href:"#"},
+    {name:"了解百度",href:"#"},
+    {name:"个人中心",href:"#"}
+  ]
+};
