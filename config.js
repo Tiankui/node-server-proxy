31c7d721b3e150a9898b0ace64691a5811ac1318
PumpingNylon <thirteenmagic@gmail.com>
2013-03-21 19:10:49 +0800

    提供RD开发模式与FE开发模式。系统有限相应启动的参数，否则按照config中配置的模式启动。

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MODE: 'fe',//RD:RD开发模式，所有请求返回JSON至浏览器||FE：FE开发模式，使用静态json数据渲染模板||DEV
+    MODE: 'dev',//RD:RD开发模式，所有请求返回JSON至浏览器||FE：FE开发模式，使用静态json数据渲染模板||DEV
     LISTEN_PORT: 3000,
     SESSION_SECRET_KEY: 'familyNodeServerSession',
     SERVER: {
