71da1a47d63112e3ffc16f248228f1a479b75f47
Tiankui <eric.prototype@gmail.com>
2013-03-22 14:09:49 +0800

    path.join-bug

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ module.exports = function (app) {
 var getRoutes = function (app, mod) {
     var routings = mod.routings;
     var moduleInfo = mod.info;
+    console.log(mod.info);
     //加载配置文件routing.js到变量routings. [/scripts/routing.js]
     for (var routing in routings) {
         //var pf = require(__dirname + routings[r].file)[routings[r].processFunction];
@@ -17,7 +18,7 @@ var getRoutes = function (app, mod) {
         }else{
             switch(app.get('env')){
                 case 'FE':
-                    var feTestService = require('../testRoutes/FeTestService')(path.normalize(moduleInfo.path+routings[routing].processFile));
+                    var feTestService = require('../testRoutes/FeTestService')(path.normalize(moduleInfo.path + routings[routing].processFile));
                     pfArray.push(feTestService,pf[pf.length-1]);
                     break;
                 case 'RD':
