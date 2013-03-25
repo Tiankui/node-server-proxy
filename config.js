e1a23f6d5814ba1f6cdcd3b75dfc1983f13ec8da
Sunyao <thirteenmagic@gmail.com>
2013-03-25 17:08:14 +0800

    socket.io+redis集群测试

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    MODE: 'dev',//RD:RD开发模式，所有请求返回JSON至浏览器||FE：FE开发模式，使用静态json数据渲染模板||DEV
+    MODE: 'fe',//RD:RD开发模式，所有请求返回JSON至浏览器||FE：FE开发模式，使用静态json数据渲染模板||DEV
     LISTEN_PORT: 3000,
-    SESSION_SECRET_KEY: 'familyNodeServerSession',
+    SESSION_SECRET_KEY: 'secret',
     SERVER: {
         HOST: '10.46.173.120',
         PORT: 8080
     },
     REDIS: {
         HOST: '172.22.234.195',
-        PORT: '6379'
+        PORT: 6379
     }
-};
+}
