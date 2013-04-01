de65d67eaeb73c9b95356e421cd66767bd4b638b
Tiankui <eric.prototype@gmail.com>
2013-04-01 20:41:46 +0800

    fe去除redis

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
  */
 
 var express = require('express'),
-    RedisStore = require('connect-redis')(express),
-    sysConfig = require('./config.js'),
-    routes = require('./routes'),
-    http = require('http'),
-    path = require('path'),
-    cluster = require('./cluster');
+RedisStore = require('connect-redis')(express),
+sysConfig = require('./config.js'),
+routes = require('./routes'),
+http = require('http'),
+path = require('path'),
+cluster = require('./cluster');
 
 
 //add ejs filters
@@ -17,50 +17,50 @@ require('./ejsFiltersAddon')(require('ejs').filters);
 var app = express();
 
 app.configure(function(){
-    app.set('views', __dirname + '/app/views');
-    app.set('view engine', 'ejs');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser());
-    app.use(express.session({
-        key: 'express.sid',
-        secret: sysConfig.SESSION_SECRET_KEY,
-        store: new RedisStore({port:sysConfig.REDIS.PORT,host:sysConfig.REDIS.HOST})
-    }));
-    app.use(app.router);
-    app.use('/lib', express['static'](__dirname + '/app/lib'));
-    app.use('/static', express['static'](__dirname + '/app/dist'));
-    app.use('/static', express['static'](__dirname + '/app/img'));
-    app.use('/test', express['static'](__dirname + '/codeLab'));
+  app.set('views', __dirname + '/app/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    key: 'express.sid',
+    secret: sysConfig.SESSION_SECRET_KEY,
+    store: new RedisStore({port:sysConfig.REDIS.PORT,host:sysConfig.REDIS.HOST})
+  }));
+  app.use(app.router);
+  app.use('/lib', express['static'](__dirname + '/app/lib'));
+  app.use('/static', express['static'](__dirname + '/app/dist'));
+  app.use('/static', express['static'](__dirname + '/app/img'));
+  app.use('/test', express['static'](__dirname + '/codeLab'));
 });
 
 
 var mode = process.argv[2]?process.argv[2]:sysConfig.MODE;
 switch (mode) {
-    case 'dev':
-        app.set('env', 'development');
-        break;
-    case 'pro':
-        app.set('env', 'production');
-        break;
-    case 'fe':
-        app.set('env', 'FE');
-        break;
-    case 'rd':
-        app.set('env', 'RD');
-        break;
-    default:
-        app.set('env', 'development');
+  case 'dev':
+    app.set('env', 'development');
+  break;
+  case 'pro':
+    app.set('env', 'production');
+  break;
+  case 'fe':
+    app.set('env', 'FE');
+  break;
+  case 'rd':
+    app.set('env', 'RD');
+  break;
+  default:
+    app.set('env', 'development');
 }
 
 app.configure('development', function () {
-    console.log('development mode');
+  console.log('development mode');
 });
 
 app.configure('production', function () {
-    console.log('production mode');
+  console.log('production mode');
 });
 
 //将app放入全局变量中
@@ -69,28 +69,28 @@ global.appContext = app;
 routes(app);
 
 /**
- http.createServer(app).listen(app.get('port'), function(){
+  http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
   });*/
 
 function startServer() {
-    var server = http.createServer(app);
-    //socket与文件上传转发服务注册 可考虑合并
-    console.log(app.get('env'));
-    if (app.get('env') !== 'FE') {
-      var socketService = require('./socket')(server);
-    }
-    var fileUploadProxy = require('./routes/fileUploadProxy')(server);
-    server.listen(sysConfig.LISTEN_PORT, function () {
-      console.log("Express server listening on port " + sysConfig.LISTEN_PORT);
-    });
+  var server = http.createServer(app);
+  //socket与文件上传转发服务注册 可考虑合并
+  console.log(app.get('env'));
+  if (app.get('env') !== 'FE') {
+    var socketService = require('./socket')(server);
+  }
+  var fileUploadProxy = require('./routes/fileUploadProxy')(server);
+  server.listen(sysConfig.LISTEN_PORT, function () {
+    console.log("Express server listening on port " + sysConfig.LISTEN_PORT);
+  });
 }
 
 
 if (app.get('env') === 'production') {
-    cluster(function () {
-        startServer();
-    });
-} else {
+  cluster(function () {
     startServer();
+  });
+} else {
+  startServer();
 }
