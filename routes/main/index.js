02b5fcc80d820e85c97890b8e299944e82faf3f9
Sunyao <thirteenmagic@gmail.com>
2013-04-02 17:41:41 +0800

    redis交集性能测试

--- routes/main/index.js
+++ routes/main/index.js
@@ -14,5 +14,7 @@ exports.routings = {
     //ajax获取首页文章列表
     '/articleList': {method: 'get',processFile: '/articleList'},
     //登录
-    '/login': {method: 'get', processFile:'/login'}
+    '/login': {method: 'get', processFile:'/login'},
+    //redis计算
+    '/sinter':{method:'get',processFile:'/sinter'}
 };
