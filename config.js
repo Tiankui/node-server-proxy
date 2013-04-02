880fbfb3b5357dcafaddef8d52ad4662177ddaac
Sunyao <thirteenmagic@gmail.com>
2013-04-02 14:30:04 +0800

    更新redis测试环境地址

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MODE: 'pro',//RD:RD开发模式，所有请求返回JSON至浏览器||FE：FE开发模式，使用静态json数据渲染模板||DEV
+    MODE: 'dev',//RD:RD开发模式，所有请求返回JSON至浏览器||FE：FE开发模式，使用静态json数据渲染模板||DEV
     LISTEN_PORT: 3000,
     SESSION_SECRET_KEY: 'secret',
     SERVER: {
@@ -7,7 +7,7 @@ module.exports = {
         PORT: 8080
     },
     REDIS: {
-        HOST: '172.22.234.189',
-        PORT: 6379
+        HOST: 'bb-art-ite07.bb01.baidu.com',
+        PORT: 8300
     }
 };
