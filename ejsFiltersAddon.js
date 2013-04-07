3aa0e683c1f00d1c3f9cf81c6cb1ee7fa1f2958d
Tiankui <eric.prototype@gmail.com>
2013-04-07 14:17:02 +0800

    view change

--- ejsFiltersAddon.js
+++ ejsFiltersAddon.js
@@ -1,6 +1,10 @@
 module.exports = function (exports) {
   exports.truncate_ = function (str, len) {
     str = String(str);
-    return str.substr(0,len) + '...';
+    if (str.length > len) {
+      return str.substr(0,len) + '...';
+    }else{
+      return str
+    }
   }; 
 };
